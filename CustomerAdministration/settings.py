5fe32d6fe4b09005f000f97e0094c37884fa67d3
Lucas Ribeiro <lucas.mr@aluno.ufop.edu.br>
2020-11-22 18:17:28 -0300

    ✨ Login and Logout

--- CustomerAdministration/settings.py
+++ CustomerAdministration/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -43,6 +44,12 @@ INSTALLED_APPS = [
 ]
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATIC_URL = '/static/'
+
+LOGIN_URL = '/login/'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -78,12 +85,13 @@ WSGI_APPLICATION = 'CustomerAdministration.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+DATABASES = {}
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+DATABASES['default'] = dj_database_url.config()
 
 
 # Password validation
--- CustomerAdministration/urls.py
+++ CustomerAdministration/urls.py
@@ -15,9 +15,14 @@ Including another URLconf
 """
 from django.urls import path, include
 from django.contrib import admin
+from django.contrib.auth.decorators import login_required
 from django.views.generic import TemplateView
+from core import views
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', TemplateView.as_view(template_name="index.html"), name="index"),
-    path("customer/", include("customer.urls"))
+    path('', login_required(TemplateView.as_view(template_name="index.html")), name="index"),
+    path("customer/", include("customer.urls")), 
+    path("login/", views.login, name="login"),
+    path("logout/", views.logout, name="logout"),
+    path("login/submit", views.submit_login, name="submit_login")
 ]
